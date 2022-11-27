f5662ff6ed970a513fe89fa83410158cb07b527d
Behike <magand.louis@gmail.com>
2022-11-27 17:08:44 +0100

    Add start and update script

---
+++ PyWord with update.bat
@@ -0,0 +1,5 @@
+title PyWord Launcher
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/main.py
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/config.py
+
+python .\main.py
