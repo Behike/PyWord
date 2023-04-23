d46684ab18f6d4c3520b6b125b9073ac2f190a40
Behike <magand.louis@gmail.com>
2023-04-23 21:20:18 +0200

    Add requirements.txt auto install in bat

--- PyWord with update.bat
+++ PyWord with update.bat
@@ -2,5 +2,6 @@ title PyWord Launcher
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/main.py
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/config.py
 
+pip install -r requirements.txt
 python .\main.py
 pause
