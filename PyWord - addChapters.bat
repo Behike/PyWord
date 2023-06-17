482abe1daf4ec3d7f885dd0017c894efa3eff96a
Behike <magand.louis@gmail.com>
2023-06-17 17:36:21 +0200

    Update requirements and add add scripts to git

--- PyWord - addChapters.bat
+++ PyWord - addChapters.bat
@@ -1,8 +1,8 @@
 title PyWord Launcher
-curl -O https://raw.githubusercontent.com/Behike/PyWord/main/main.py
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/addChapter.py
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/config.py
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/requirements.txt
 
 pip install -r requirements.txt
-python .\main.py
+python .\addChapter.py
 pause
---
+++ PyWord - main.bat
@@ -0,0 +1,8 @@
+title PyWord Launcher
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/main.py
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/config.py
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/requirements.txt
+
+pip install -r requirements.txt
+python .\main.py
+pause
