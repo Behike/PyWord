aa345d6cae9fea50a0ab05c8b681b8c18dbae522
Behike <magand.louis@gmail.com>
2023-06-19 18:02:53 +0200

    Add Style files in batch scripts

--- PyWord - docxToEpub.bat
+++ PyWord - docxToEpub.bat
@@ -2,6 +2,10 @@ title PyWord Launcher
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/docxToEpub.py
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/config.py
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/requirements.txt
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/requirements.txt
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/Style/default.css
+curl -O https://github.com/Behike/PyWord/raw/main/Style/Cambria-Font.ttf
+curl -O https://github.com/Behike/PyWord/raw/main/Style/Palatino%20Linotype.ttf
 
 pip install -r requirements.txt
 python .\docxToEpub.py
--- PyWord - main.bat
+++ PyWord - main.bat
@@ -2,6 +2,9 @@ title PyWord Launcher
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/main.py
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/config.py
 curl -O https://raw.githubusercontent.com/Behike/PyWord/main/requirements.txt
+curl -O https://raw.githubusercontent.com/Behike/PyWord/main/Style/default.css
+curl -O https://github.com/Behike/PyWord/raw/main/Style/Cambria-Font.ttf
+curl -O https://github.com/Behike/PyWord/raw/main/Style/Palatino%20Linotype.ttf
 
 pip install -r requirements.txt
 python .\main.py
